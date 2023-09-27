579942e3cb165041107fba391830a12f356cf2ce
AartiShah1 <143744217+AartiShah1@users.noreply.github.com>
2023-09-27 11:32:08 +0000

    datype

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -13,8 +13,29 @@ console.log(valueinNumber);
 // "33"-33       
 // "33sdcc"--NaN
 // true-1;  false-0
+
+
 // Conversion in boolean
+/*
 let isLoggedIn=1
 let booleanisLoggedin=Boolean(isLoggedIn);
 console.log( typeof booleanisLoggedin);
 console.log(booleanisLoggedin);
+*/
+
+//Chnge the value into negative
+/*
+let value=5;
+let negnum=-value;
+console.log(negnum);
+*/
+
+
+let str1="Hello";
+let str2=" Aarti";
+//console.log(str1+str2);
+let str3= str1+str2;
+console.log(str3);
+
+console.log(4+"2"+3);
+console.log(((4+2)-3)*2);               // in this case Use parenthesis 
