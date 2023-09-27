5b956672a672e683702030f6feae0563df991578
AartiShah1 <143744217+AartiShah1@users.noreply.github.com>
2023-09-27 10:12:43 +0000

    config

--- 01_basic/02_dataTypes.js
+++ 01_basic/02_dataTypes.js
@@ -5,3 +5,26 @@
 
 console.log(3+3)
 console.log("Aarti")
+
+let name="Aarti"
+let age=15
+let isLoggedIn =false
+
+/*
+there are different types of datatype
+number=>2 to power53
+BigInt- for larger number
+boolean- true or false
+null -standalone value or representation of empty
+undefined- varible declared but not put the value;
+symbol-unique
+*/
+
+// Object
+console.log(typeof "Aarti");               //you can know typeof variable
+console.log(typeof 15);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof false);
+console.log(typeof undefined);
+console.log(typeof null);
