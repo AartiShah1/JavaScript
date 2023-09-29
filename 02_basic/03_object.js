b26b20c19577f74156f84bcdfe5a2163f6f9d375
AartiShah1 <143744217+AartiShah1@users.noreply.github.com>
2023-09-29 09:59:14 +0000

    object

--- 02_basic/03_object.js
+++ 02_basic/03_object.js
@@ -7,12 +7,12 @@ const mySym = Symbol("key1")
 
 
 const JsUser = {
-    name: "Hitesh",
-    "full name": "Hitesh Choudhary",
+    name: "Aarti",
+    "full name": "Aarti shah",
     [mySym]: "mykey1",
     age: 18,
     location: "Jaipur",
-    email: "hitesh@google.com",
+    email: "aarti@google.com",
     isLoggedIn: false,
     lastLoginDays: ["Monday", "Saturday"]
 }
@@ -22,9 +22,9 @@ const JsUser = {
 // console.log(JsUser["full name"])
 // console.log(JsUser[mySym])
 
-JsUser.email = "hitesh@chatgpt.com"
+JsUser.email = "aarti@chatgpt.com"
 // Object.freeze(JsUser)
-JsUser.email = "hitesh@microsoft.com"
+JsUser.email = "aarti@microsoft.com"
 // console.log(JsUser);
 
 JsUser.greeting = function(){
