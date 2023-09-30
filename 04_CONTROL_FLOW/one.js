103c98f1daf04634fb1dded32bf3b2944b9e87db
AartiShah1 <143744217+AartiShah1@users.noreply.github.com>
2023-09-30 03:45:32 +0000

    DOM

---
+++ 04_CONTROL_FLOW/one.js
@@ -0,0 +1,16 @@
+if(2==="2"){
+    console.log("executed");
+}
+if(2!=5){
+    console.log("executed");
+}
+
+
+const temperature=12;                  // assigning the user value
+if(temperature<50)
+{
+    console.log("temperature is less tha 50");
+}
+else{
+    console.log("temperature is greater tha 50");
+}
